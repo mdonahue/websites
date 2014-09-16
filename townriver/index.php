bea44c7f39abf53bb51583fa67f676aef05000c7
Mark Donahue <mark-donahue@idexx.com>
2014-09-15 21:24:05 -0400

    migration from svn

---
+++ townriver/index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta name="verify-v1" content="YAWRXwDNeKXSTG+x8HOwruNhKn32vEG0pedugrwfIII="/>
+    <title>Town River Marina - 674 Southern Artery Quincy, Massachusetts</title>
+<style type="text/css">
+        <!--
+        @import url(standard.css);
+        -->
+    </style>
+	</head>
+<body>
+
+<div class="container">
+    <div class="panorama">
+        <img alt="Town River Marina" src="images/panorama.jpg" width="640" height="125"/>
+    </div>
+    <div class="marinaname">
+        Town River Marina
+    </div>
+
+    <?php include 'navigation.php'; ?>
+	<div class="bodytext">
+        <div class="lede">
+            <h4 class="callout">2014 Dockage/Mooring at Town River Marina.</h4>
+
+            <p>To get started: </p>
+            <ol>
+                <li>Fill out the License documents and sign where indicated.</li>
+                <li>Include deposit and insurance certificate.</li>
+                <li>Return by postal or e-mail.</li>
+                <li>Once your complete package has been received, we will contact you with your space assignment.</li>
+            </ol>
+			<p>Winter: Storage</p>
+            <ul>
+				<li><a href="docs/storage/EXHIBIT_ALICENSE.doc">Exhibit A Boat Storage License</a></li>
+				<li><a href="docs/storage/licagreestorage_secI.doc">Storage License</a></li>
+				<li><a href="docs/storage/licagreestoragesecII.doc">Storage License Section II</a></li>
+                <li><a href="docs/BMPs2009.doc">Best Management Practices, Rules and Regulations</a></li>
+            </ul>
+			<p>Summer: Dockage</p>
+			<ul>
+			    <li><a href="docs/dockage/EXHIBIT_A_DOCKAGE_LICENSE2010.doc">Exhibit A Boat Dockage License</a></li>
+                <li><a href="docs/dockage/mooringagreement2010.doc">Mooring Agreement, Section I</a></li>
+                <li><a href="docs/dockage/Dockageagreement2011.rtf">License Agreement, Section I</a></li>
+                <li><a href="docs/dockage/sectionII2010.DOC">License Agreement, Section II</a></li>
+				<li><a href="docs/BMPs2009.doc">Best Management Practices, Rules and Regulations</a></li>
+			</ul>
+        </div>
+
+        <p>Town River Marina is located at the sheltered head of Town River Bay in Quincy,
+            Massachusetts off the Fore River. We are a family owned and operated facility
+            that caters to individuals interested in sport fishing and family oriented
+            boating.</p>
+
+        <p>The services we offer include summer dockage, mooring rentals and servicing and
+            a launching ramp with good access to Boston Harbor and Massachusetts Bay. Dry
+            storage is available during both winter and summer seasons for power and sailing
+            vessels.</p>
+
+        <p>
+            <img src="images/bayliss.jpg" alt="Johyn Bayliss, Master technician" width="447" height="295">
+            <br/>John Bayliss, Master Technician
+        </p>
+        <ul>
+            <li>Complete Outboard Service</li>
+            <li>Mercruiser, Volvo Penta Service and Repowering</li>
+            <li>Electronics Installation</li>
+            <li>Mobile Service available</li>
+            <li>Bottom Paint and Waxing</li>
+            <li>Complete Trailer Repair</li>
+            <li>Winterizing and Spring Commissioning</li>
+        </ul>
+
+        <p><a href="http://www.mass.gov/czm/nda/pumpouts/boston__harbor_pumpouts.htm">Boston Harbor Pumpout guide</a>
+        </p>
+
+        <p>Incorporated into our facility are additional services which include:</p>
+
+        <div class="tennant">
+            <p><span class="callout">The Sportsman's Den</span> - (617) 770 - 3884
+            <ul>
+                <li>A Complete line of Saltwater and Freshwater tackle</li>
+                <li>Fresh and Frozen bait</li>
+                <li>Rod and Reel repair</li>
+                <li><a href="http://www.stcroixrods.com/" target="_new">St. Croix</a> and
+                    <a href="http://www.lamiglas.com/">Lamiglass</a> rod dealer.
+                </li>
+                <li>Open 7 days</li>
+            </ul>
+            </p>
+        </div>
+        <div class="tennant">
+            <p><span class="callout">Bill &amp; Jules Boat and Shore Fishing Specialists</span> - (617) 633-6738</p>
+
+            <p>Captain William "Bill" Bryant (USCG Licensed Master Captain) and Julianne " Jules" Silvis offer Shore
+                trips, charters on their 2005 27 ft Polar Center Console, named the Alternative. <a
+                        href="http://billandjulsfishing.com/">http://billandjulsfishing.com</a></p>
+
+            <p><span class="callout">GoFish Sportfishing Charters</span>- (508) 395-4767</p>
+
+            <p>Captain Patrick Helsingius (USCG Licensed Master Captain) specializes in light tackle fishing for striped
+                bass and bluefish in Boston Harbor. <a href="http://www.gofishboston.com">www.gofishboston.com</a></p>
+        </div>
+    </div>
+
+    <div id="mmta" class="ratesheader">
+        <a href="http://www.boatma.com/index.html"><img alt="MA Marine Trade Association logo"
+                                                        src="http://www.boatma.com/images/logos/mmta_logo.gif"
+                                                        width="312" border="0"></a>
+    </div>
+
+    <div class="address">
+        Town River Marina<br/>
+        674 Southern Artery<br/>
+        Quincy, MA 02169<br/>
+        P.O. Box 502<br/>
+        Scituate, MA 02066<br/>
+        <a href="mailto:marina@townriver.com">marina@townriver.com</a><br/>
+        Telephone: 617-745-9813<br/>
+    </div>
+
+</div>
+</body>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+    _uacct = "UA-2358665-2";
+    urchinTracker();
+</script>
+</html>
---
+++ townriver/links.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>
+      Town River Marina - Mooring and Yacht Services in Quncy, MA: (Untitled)
+    </title>
+    <meta name="description" content="Town River Marina provides mooring services as well as yacht storage and launching facilities on the Twon River in Quincy, MA" />
+    <meta name="keywords" content="mooring services, yacht brokerage, yacht storage, Quincy Massachusetts" />
+    <meta name="ICBM" content="42.2523,-70.9871" />
+    <meta name="DC.title" content="Town River Marina - Mooring and Yacht Services on the Town River in Quincy, MA" />
+    <style type="text/css">
+       <!--
+       @import url(standard.css);
+       -->
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="panorama">
+         <img alt="Town River Marina" src="images/panorama.jpg" width="640" height="125" />
+      </div>
+      <div class="marinaname">
+         Town River Marina
+      </div>
+
+    <?php include 'navigation.php'; ?>
+
+      <div class="bodytext">
+        <div class="ratesheader">
+          Marine Links
+        </div>
+        <ul>
+          <li>
+            <a href="http://www.boatma.com">http://www.boatma.com</a>
+          </li>
+          <li>
+            <a href="http://www.maineharbors.com/ma/malnk.htm">http://www.maineharbors.com/ma/malnk.htm</a>
+          </li>
+          <li>
+            <a href="http://www.by_the_sea.com">http://www.by_the_sea.com</a>
+          </li>
+          <li>
+            <a href="http://www.bostonboating.com">http://www.bostonboating.com</a>
+          </li>
+        </ul>
+      </div>
+      <div class="address">
+         Town River Marina<br/>
+         674 Southern Artery<br/>
+         Quincy, MA 02169<br/>
+         P.O. Box 164<br/>
+         Hingham, MA 02043<br/>
+         <a href="mailto:marina@townriver.com">marina@townriver.com</a><br/>
+         Telephone: 617-745-9813<br/>
+      </div>
+       <a href="http://geourl.org/near/?p=http://townriver.com/index.html" title="What's nearby Town River Marina"> <img src="http://geourl.org/geourl.png" border="0" width="52" height="14" /></a>
+    </div>
+  </body>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-2358665-2";
+urchinTracker();
+</script></html>
