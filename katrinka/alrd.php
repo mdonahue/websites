0d109bdf7da01f7067e424052812a459ecf885e8
Mark Donahue <mark-donahue@idexx.com>
2014-09-15 21:27:02 -0400

    migration from svn

---
+++ katrinka/alrd.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ALRD Web Services Client</title>
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le styles -->
+	<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+      }
+    </style>
+</head>
+<body>
+<?php
+phpinfo();
+?>
+<p>PHP, FTW!</p>
+</body>
+</html>
