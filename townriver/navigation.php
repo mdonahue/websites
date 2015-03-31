3fc8f660b1ce95fcb2e4a60de89406abdf508c7c
mdonahue <mark@katrinka.net>
2015-03-30 22:34:50 -0400

    address change and summer 2015

--- townriver/navigation.php
+++ townriver/navigation.php
@@ -1,7 +1,7 @@
       <div class="nav">
          <a href="index.php">Home</a>
          | <a href="brokerage.php">Brokerage</a>
-         | <a href="summer-rates.php">Summer 2014 Rates</a>
+         | <a href="summer-rates.php">Summer 2015 Rates</a>
          | <a href="winter-rates.php">Winter 2014-2015 Rates</a>
          | <a href="links.php">Links</a>
          |<a href="services.php">Mooring Services</a>
